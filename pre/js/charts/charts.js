2ee321a3efde3f71079ffd401c352cca91ff93a5
Carlos Munoz Diaz <carlos.munoz@cchs.csic.es>
2022-04-03 19:41:38 +0200

    Primer desarrollo del gráfico + Cambio en JSON

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,5 +1,4 @@
 //Desarrollo de las visualizaciones
-import * as d3 from 'd3';
 import { numberWithCommas2 } from '../helpers';
 //import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
 import { setChartHeight } from '../modules/height';
@@ -21,10 +20,76 @@ COLOR_OTHER_2 = '#731854';
 
 export function initChart(iframe) {
     //Desarrollo del gráfico
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_4/main/data/json_sankey_quien_cuida_a_quien.json', function(error,data) {
+    d3.json('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_4/main/data/json_sankey_quien_cuida_a_quien.json', function(error,data) {
         if (error) throw error;
 
-        console.log(data);
+        let margin = {top: 10, right: 5, bottom: 10, left: 5},
+            width = document.getElementById('chart').clientWidth - margin.left - margin.right,
+            height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
+
+        let svg = d3.select("#chart")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        let sankey = d3.sankey()
+            .nodeWidth(15)
+            .nodePadding(10)
+            .extent([[1, 1], [width - 1, height - 6]]);
+
+        let link = svg.append("g")
+            .attr("class", "links")
+            .attr("fill", "none")
+            .attr("stroke", "#000")
+            .attr("stroke-opacity", 0.2)
+            .selectAll("path");
+
+        let node = svg.append("g")
+            .attr("class", "nodes")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .selectAll("g");
+
+        let graph = sankey(data);
+
+        let color = d3.scaleOrdinal()
+            .domain(['Hombres < 65','Hombres 65+','Mujeres < 65','Mujeres 65+'])
+            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_OTHER_1]);
+
+        function init() {
+
+            link = link
+                .data(data.links)
+                .enter()
+                .append("path")
+                .attr("d", d3.sankeyLinkHorizontal())
+                .attr("stroke-width", function(d) { return Math.max(1, d.width); });
+
+            node = node
+                .data(data.nodes)
+                .enter()
+                .append("g");
+
+            node.append("rect")
+                .attr("x", function(d) { return d.x0; })
+                .attr("y", function(d) { return d.y0; })
+                .attr("height", function(d) { return d.y1 - d.y0; })
+                .attr("width", function(d) { return d.x1 - d.x0; })
+                .attr("fill", function(d) { console.log(d); return 'red'; });
+
+            node.append("text")
+                .attr("x", function(d) { return d.x0 - 6; })
+                .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+                .attr("dy", "0.35em")
+                .attr("text-anchor", "end")
+                .text(function(d) { return d.name; })
+                .filter(function(d) { return d.x0 < width / 2; })
+                .attr("x", function(d) { return d.x1 + 6; })
+                .attr("text-anchor", "start");
+
+        }
 
         /////
         /////
@@ -42,11 +107,7 @@ export function initChart(iframe) {
         /////
         // Resto
         /////
-        /////
-        //Animación del gráfico
-        document.getElementById('replay').addEventListener('click', function() {
-            animateChart();
-        });
+        /////        
 
         //Iframe
         setFixedIframeUrl('informe_perfil_mayores_2022_social_4_4','patrones_cuidado_informal');
