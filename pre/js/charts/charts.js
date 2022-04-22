ac0ae0ade0b0853ae168a1e9c20fd803ff9e1a92
Carlos Munoz Diaz <carlos.munoz@cchs.csic.es>
2022-04-22 10:39:30 +0200

    Primer desarrollo serio completado

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,29 +1,24 @@
 //Desarrollo de las visualizaciones
-import { numberWithCommas2 } from '../helpers';
-//import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
+import { numberWithCommas3 } from '../helpers';
+import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42', 
-COLOR_ANAG_1 = '#D1834F', 
-COLOR_ANAG_2 = '#BF2727', 
-COLOR_COMP_1 = '#528FAD', 
-COLOR_COMP_2 = '#AADCE0', 
-COLOR_GREY_1 = '#B5ABA4', 
-COLOR_GREY_2 = '#64605A', 
-COLOR_OTHER_1 = '#B58753', 
-COLOR_OTHER_2 = '#731854';
+const COLOR_PRIMARY_1 = '#F8B05C',
+COLOR_COMP_2 = '#AADCE0',
+COLOR_ANAG__PRIM_1 = '#BA9D5F',
+COLOR_ANAG_COMP_1 = '#1C5A5E';
+let tooltip = d3.select('#tooltip');
 
 export function initChart(iframe) {
     //Desarrollo del gráfico
     d3.json('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_4/main/data/json_sankey_quien_cuida_a_quien.json', function(error,data) {
         if (error) throw error;
 
-        let margin = {top: 0, right: 0, bottom: 0, left: 0},
+        let margin = {top: 5, right: 0, bottom: 0, left: 0},
             width = document.getElementById('chart').clientWidth - margin.left - margin.right,
             height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
 
@@ -43,7 +38,7 @@ export function initChart(iframe) {
             .attr("class", "links")
             .attr("fill", "none")
             .attr("stroke", "#000")
-            .attr("stroke-opacity", 0.2)
+            .attr("stroke-opacity", 0.5)
             .selectAll("path");
 
         let node = svg.append("g")
@@ -52,19 +47,50 @@ export function initChart(iframe) {
             .attr("font-size", 10)
             .selectAll("g");
 
-        let graph = sankey(data);
+        sankey(data);
 
         let color = d3.scaleOrdinal()
             .domain(['Hombres < 65','Hombres 65+','Mujeres < 65','Mujeres 65+'])
-            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_OTHER_1]);
+            .range([COLOR_PRIMARY_1, COLOR_ANAG__PRIM_1, COLOR_COMP_2, COLOR_ANAG_COMP_1]);
 
         function init() {
             link = link
                 .data(data.links)
                 .enter()
                 .append("path")
+                .attr('class', 'rect')
                 .attr("d", d3.sankeyLinkHorizontal())
-                .attr("stroke-width", function(d) { return Math.max(1, d.width); });
+                .attr("stroke-width", function(d) { return Math.max(1, d.width); })
+                .attr('stroke', function(d) { return color(d.source.name); })
+                .on('mouseover', function(d,i,e) {
+                    console.log(d);
+
+                    //Opacidad del link señalado
+                    this.style.strokeOpacity = 1;
+
+
+
+                    //Texto en tooltip
+                    let html = '<p class="chart__tooltip--title_1">Horas de cuidado: ' + numberWithCommas3(parseInt(d.value)) + '</p>' +
+                        '<p class="chart__tooltip--title_2">Cuidador: ' + d.source.name + '</p>' + 
+                        '<p class="chart__tooltip--text">El <b>' + numberWithCommas3((parseInt(d.value) / parseInt(d.source.value) * 100).toFixed(1)) + '%</b> del tiempo cuidan a estas personas</p>' +
+                        '<p class="chart__tooltip--title_3">Dependiente: ' + d.target.name + '</p>' + 
+                        '<p class="chart__tooltip--text">El <b>' + numberWithCommas3((parseInt(d.value) / parseInt(d.target.value) * 100).toFixed(1)) + '%</b> del tiempo que reciben cuidados, lo reciben por parte de este tipo de cuidador</p>';
+                
+                    tooltip.html(html);
+
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+
+                })
+                .on('mouseout', function(d,i,e) {
+                    //Opacidad del link señalado
+                    this.style.strokeOpacity = 0.5;
+
+                    //Fuera tooltip
+                    getOutTooltip(tooltip);
+                })
 
             node = node
                 .data(data.nodes)
@@ -78,17 +104,27 @@ export function initChart(iframe) {
                 .attr("width", function(d) { return d.x1 - d.x0; })
                 .attr("fill", function(d) { return color(d.name); });
 
-            node.append("text")            
+            node.append('text')     
                 .attr('class','sankey-text')
                 .attr("x", function(d) { return d.x0 - 6; })
-                .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+                .attr("y", function(d) { return (d.y1 + d.y0) / 2 - 10; })
                 .attr("dy", "0.35em")
                 .attr("text-anchor", "end")
-                .text(function(d) { return d.name + ' (' + d.value + ')'; })
+                .text(function(d) { return d.name; })
                 .filter(function(d) { return d.x0 < width / 2; })
                 .attr("x", function(d) { return d.x1 + 6; })
-                .attr("text-anchor", "start");
+                .attr("text-anchor", "start")
 
+            node.append('text')
+                .attr('class','sankey-text')
+                .attr("x", function(d) { return d.x0 - 6; })
+                .attr("y", function(d) { return (d.y1 + d.y0) / 2 + 10; })
+                .attr("dy", "0.35em")
+                .attr("text-anchor", "end")
+                .text(function(d) { return ' (' + numberWithCommas3(parseInt(d.value)) + ' horas)'; })
+                .filter(function(d) { return d.x0 < width / 2; })
+                .attr("x", function(d) { return d.x1 + 6; })
+                .attr("text-anchor", "start")
         }
 
         /////
